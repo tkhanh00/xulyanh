6793913bc2537c2bfea59d7de27832e6e9e10b84
tkhanh00 <85058203+tkhanh00@users.noreply.github.com>
2022-11-27 09:46:53 +0700

    Add files via upload

---
+++ code/Hello.py
@@ -0,0 +1,35 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Hello",
+    page_icon="👋",
+)
+
+st.write("# Welcome to Streamlit! 👋")
+st.write("Listen : Open Project App To View Full Project Important (LINK : https://slytherinsecond-python-deploy-streamlit-pagesapp-8sbcym.streamlit.app/)")
+st.write("Project Run by API Streamlits . Please Start project with wifi good or strong")
+st.write("if cannot run project when start . Please run again")
+st.write("Project Code by 3 member")
+st.write("Pham The Anh")
+st.write("Ho Truong Cong Thang")
+st.write("Le Thuy Duong")
+st.write("Tks Mr.Duc Training & Support")
+st.sidebar.success("Select a demo above.")
+
+st.markdown(
+    """
+    Streamlit is an open-source app framework built specifically for
+    Machine Learning and Data Science projects.
+    **👈 Select a demo from the sidebar** to see some examples
+    of what Streamlit can do!
+    ### Want to learn more?
+    - Check out [streamlit.io](https://streamlit.io)
+    - Jump into our [documentation](https://docs.streamlit.io)
+    - Ask a question in our [community
+        forums](https://discuss.streamlit.io)
+    ### See more complex demos
+    - Use a neural net to [analyze the Udacity Self-driving Car Image
+        Dataset](https://github.com/streamlit/demo-self-driving)
+    - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
+"""
+)
